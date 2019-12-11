9cda12ce53f611f28b2b6f78e3ab0ba72b8dddc2
Tyrese Samuel <3tyrese@gmail.com>
2019-12-11 10:11:45 -0600

    Add project files.

---
+++ GraphicalTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(1280, 760, "Tank Trouble 0.5");
+
+            Actor root = new Actor();
+            game.Root = root;
+
+            game.Run();
+        }
+    }
+}
---
+++ GraphicalTestApp/Turret.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalTestApp
+{
+    class Turret : Actor
+    {
+        private Sprite _turret;
+
+        public Turret(float x, float y)
+        {
+            _turret = new Sprite("tankDark_barrel2_outline.png");
+
+            _turret.X = -8.0f;
+            _turret.Y = -45;
+
+            X = 0;
+            Y = 0;
+
+            AddChild(_turret);
+
+
+            OnUpdate += Rotateleft;
+            OnUpdate += Rotateright;
+        }
+
+        public void Rotateleft(float deltatime)
+        {
+            //Rotates the Turret to the left (J)
+            if (Input.IsKeyDown(74))
+            {
+
+                Rotate(-1f * deltatime);
+            }
+        }
+
+
+        public void Rotateright(float deltatime)
+        {
+            //Rotates the Turret to the right (L)
+            if (Input.IsKeyDown(76))
+            {
+
+                Rotate(1f * deltatime);
+            }
+        }
+
+        public void Fire()
+        {
+
+        }
+    }
+}
